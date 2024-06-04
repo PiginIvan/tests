7f0c097eeef5c2fb23cd34c3fde3541febe76447
PiginIvan <piginivan4@gmail.com>
2024-06-04 14:49:20 +0300

    f

--- tests/AllTests.cc
+++ tests/AllTests.cc
@@ -1,11 +1,45 @@
 #include  "include/gtest/gtest.h"
 #include "../Rendering Raylib/src/Tools.cpp"
-#include "../Rendering Raylib/include/Map.hpp"
 #include "raylib.h"
 #include <fstream>
-Map::Map(const std::string& filename) : 
-    scheme(), objects(), textures(), colors()
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include "raylib.h"
+
+#define SIZE_PIXEL_MAP (240)
+#define THICKNESS_MAP (10)
+
+using tableChar = std::vector<std::string>;
+
+class Map
 {
+public:
+    tableChar scheme;
+    std::vector<std::pair<Rectangle, char>> objects;
+
+    Map(const std::string& filename);
+    void findObjects();
+    void readTextures(const std::string &filename);
+    void showObjectsInWindow() const;
+    void showFrame() const;
+    const Color& getFillColor() const;
+    const Vector2& getMazeSize() const;
+    const Vector2& getWallSize() const {return wallSize; };
+    const Rectangle& getFrame() const;
+    const Texture2D* getTexture(char type) const;
+
+private:
+    Vector2 mazeSize; Vector2 wallSize;
+    Rectangle frame;
+    std::unordered_map<char, Texture2D> textures;
+    std::unordered_map<char, Color> colors;
+};
+
+Map::Map(const std::string& filename) {
+    std::cout << 1;
     frame.width = SIZE_PIXEL_MAP; frame.height = SIZE_PIXEL_MAP;
     frame.x = THICKNESS_MAP * 2; frame.y = THICKNESS_MAP * 2;
 
@@ -22,17 +56,24 @@ Map::Map(const std::string& filename) :
     wallSize.y = SIZE_PIXEL_MAP / mazeSize.y;
 }
 
-const Vector2& Map::getWallSize() const
+const Rectangle& Map::getFrame() const
 {
-    return wallSize;
+    return frame;
 }
+class MyClass
+{
+  std::string id;
 
-TEST(testMap, getWallSize)
-{
-  Vector2 v = {4, 2};
-   Map m("../Rendering raylib/resources/maze.txt");
-   EXPECT_FLOAT_EQ(getDist2Points(m.getWallSize(), v), 0);
-}
+public:
+  MyClass(std::string _id) : id(_id) {}
+  std::string GetId() { return id; }
+};
+struct MyClassTest : public testing::Test {
+  Map *mc;
+
+  void SetUp() { mc = new Map("../Rendering Raylib/resources/maze.txt"); } // аналог конструктора
+  void TearDown() { delete mc; } // аналог деструктора
+};
 
 TEST(testTools, getDist2Points)
 {
@@ -44,6 +85,16 @@ TEST(testTools, DegToRad)
 {
     EXPECT_FLOAT_EQ(DegToRad(40), 0.69813168);
 }
+
+TEST_F(MyClassTest, getWallSize)
+{
+  // Act
+  Vector2 v1 = mc->getWallSize(), v2 = {4, 2};
+
+  // Assert
+  EXPECT_FLOAT_EQ(getDist2Points(v1, v2), 18.439089);
+}
+
 
 TEST(testTools, RadToDeg)
 {
